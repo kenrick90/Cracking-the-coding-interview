37b826ad43a8e9355854982940cf8153779219c8
unknown <kenrickng90@gmail.com>
2020-12-13 16:30:19 +0800

    Working on Equal

--- HackerRank/Equal.py
+++ HackerRank/Equal.py
@@ -1,21 +1,61 @@
 # https://www.hackerrank.com/challenges/equal/problem
 
-def minOperationEqual(arr,n):
-	Equal = False
+def determineOperation(diff):
+	if diff >= 5:
+		return 5
+	if diff >= 2:
+		return 2
+	return 1
 
-	maximum = arr[0]
-	minimum = arr[0]
+def minOperationEqual(arr,n):
+	secondMaximum = maximum = minimum = arr[0]
+	secondmaxIndex = maxIndex = minIndex = 0
+
 	for i in range(n):
 		if arr[i] > maximum:
+			secondmaxIndex = maxIndex
+			secondMaximum = maximum
 			maxIndex = i
 			maximum = arr[i]
+			continue
+
+		if arr[i] > secondMaximum:
+			secondmaxIndex = i
+			secondMaximum = arr[i]
+			continue
+
 		if arr[i] < minimum:
 			minIndex = i
 			minimum = arr[i]
+			continue
+
+	operation = 0
+	#maximum = 3 maxIndex = 2
+	#minimum = 2 minIndex = 0
+	#operation = 1
+	# print(maximum,secondMaximum,minimum)
+
+	while maximum != minimum:
+
+		# print(maximum,maxIndex)
+		arr[maxIndex] -= determineOperation(maximum - minimum)
+		operation += 1
+		#case 1
+		if arr[maxIndex] < secondMaximum:
+			prevSecondMaxIndex = secondmaxIndex
+			prevSecondMaximum = secondMaximum
+			secondmaxIndex = maxIndex
+			secondMaximum = arr[maxIndex]
+			maxIndex = prevSecondMaxIndex
+			maximum = prevSecondMaximum
+		else:
+			maximum = arr[maxIndex]
+	return operation
 
 
 if __name__ == "__main__":
 	t = int(input())
-	n = int(input())
-	arr = list(map(int,input().rstrip().split()))
-	return minOperationEqual(arr,n)
+	for i in range(t):
+		n = int(input())
+		arr = list(map(int,input().rstrip().split()))
+		print(minOperationEqual(arr,n))
